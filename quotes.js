83ed242b86e1708bf6e31984b0af23681f5d29b3
Darren Benn <darren.benn@digitalcontact.co.uk>
2015-12-29 20:40:09 +0000

    Initial commit

---
+++ quotes.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var http = require('http');
+
+module.exports = function(ctx, done) {
+    
+    getQuotes(ctx.data.tickers);
+    
+    function getQuotes(tickerSymbols) {
+        http.get({
+        host: 'www.google.com',
+        port: 80,
+        path: '/finance/info?client=ig&q=' + tickerSymbols
+    }, function(response) {
+        response.setEncoding('utf8');
+        var data = "";
+                    
+        response.on('data', function(chunk) {
+            data += chunk;
+        });
+        
+        response.on('end', function() {
+            if(data.length > 0) {
+                try {
+                    var data_object = JSON.parse(data.substring(3));
+                } catch(e) {
+                    return done(e);
+                }
+                var quotes = [];
+                
+                for(var i = 0; i < data_object.length; i++) {
+                    var quote = {};
+                    
+                    quote.ticker = data_object[i].t;
+                    quote.exchange = data_object[i].e;
+                    quote.price = data_object[i].l_cur;
+                    quote.change = data_object[i].c;
+                    quote.change_percent = data_object[i].cp;
+                    quote.last_trade_time = data_object[i].lt;
+                    quote.dividend = data_object[i].div;
+                    quote.yield = data_object[i].yld;   
+                    
+                    quotes.push(quote);
+                }
+                
+                return done(null, quotes);
+            }
+        });
+    })
+}};
